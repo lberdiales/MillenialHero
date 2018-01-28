d23d6a4ed7b17304cac7099c735f47161e9c9c3d
Lucas Berdiales <lberdiales@gmail.com>
2018-01-28 01:43:41 -0300

    Added desperation to Player, now he desperates when losing wifi or battery

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,16 @@ extends KinematicBody2D
 
 onready var player_sprite = get_node("sprite")
 
+# Status
+export var MAX_DESPERATION = 100.0
+var desperation = 0
+
+export var TIME_TO_PROCESS_STATUS = 2
+var status_delta_acum = 0.0
+
+export var DESPERATION_DUE_TO_NO_WIFI = 5
+export var DESPERATION_DUE_TO_NO_BATTERY = 1
+
 # Movement
 var player_speed = 100
 var player_step_freq = 10
@@ -21,7 +31,38 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	process_status(delta)
 	process_input(delta)
+
+# Status
+func process_status(delta):
+	status_delta_acum += delta
+	
+	if (status_delta_acum >= TIME_TO_PROCESS_STATUS):
+		var desperation_due_to_no_battery = process_battery_desperation()
+		var desperation_due_to_no_wifi = process_wifi_desperation()
+		
+		desperation += desperation_due_to_no_wifi + desperation_due_to_no_battery
+		
+		if (desperation >= MAX_DESPERATION):
+			die()
+		else:
+			# FIXME: Arreglar
+			status_delta_acum = 0
+
+func process_battery_desperation():
+	return DESPERATION_DUE_TO_NO_BATTERY
+
+func process_wifi_desperation():
+	var signal_strength = 0.0
+	if (current_hotspot != null):
+		signal_strength = current_hotspot.signal_strength(self)
+	
+	return DESPERATION_DUE_TO_NO_WIFI - ((signal_strength / 100) * DESPERATION_DUE_TO_NO_WIFI)
+
+func die():
+	# Pendiente implementar
+	pass
 
 # Movement
 func process_input(delta):
@@ -65,7 +106,7 @@ func process_input(delta):
 
 # Hotspot
 func _on_hotspot_enter( hotspot ):
-	if(!hotspots.has(hotspot)):
+	if (!hotspots.has(hotspot)):
 		hotspots.push_front(hotspot)
 	process_hotspots()
 
@@ -75,11 +116,13 @@ func _on_hotspot_exit( hotspot ):
 
 func process_hotspots():
 	# Aca hay que revisar cual conviene mas y conectar a ese (usando hotspot.signal_strength())
-	if(hotspots.size()==0):
+	if (hotspots.empty()):
 		current_hotspot = null
 		return
-		
-	if(current_hotspot == null): current_hotspot = hotspots.front()
+	
+	if (current_hotspot == null):
+		current_hotspot = hotspots.front()
+	
 	for hotspot in hotspots:
-		if(hotspot.signal_strength(self) >current_hotspot.signal_strength(self)):
+		if (hotspot.signal_strength(self) > current_hotspot.signal_strength(self)):
 			current_hotspot = hotspot
