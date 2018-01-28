09c3f74efbe43ea076ac201cb8099003a8148174
Francisco <francisco_casanova@hotmail.es>
2018-01-27 23:36:25 -0300

    Adds signal strength calculation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-var hotspots = {}
+var hotspots = []
+var current_hotspot
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -8,15 +9,21 @@ func _ready():
 	pass
 
 func _on_hotspot_enter( hotspot ):
-	hotspots[hotspot.hotspot_id] = hotspot
+	if(!hotspots.has(hotspot)):
+		hotspots.push_front(hotspot)
 	process_hotspots()
 
-
 func _on_hotspot_exit( hotspot ):
-	hotspots.erase(hotspot.hotspot_id)
+	hotspots.erase(hotspot)
 	process_hotspots()
 
 func process_hotspots():
 	# Aca hay que revisar cual conviene mas y conectar a ese (usando hotspot.signal_strength())
-	if (hotspots.size() > 0):
-		hotspots.values().front().connect()
+	if(hotspots.size()==0):
+		current_hotspot = null
+		return
+		
+	if(current_hotspot == null): current_hotspot = hotspots.front()
+	for hotspot in hotspots:
+		if(hotspot.signal_strength(self) >current_hotspot.signal_strength(self)):
+			current_hotspot = hotspot
--- Scripts/hotspot.gd
+++ Scripts/hotspot.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
 onready var hotspot_id = get_name()
+onready var collider = get_child(0)
+onready var top = get_node('top')
+onready var side = get_node('side')
+onready var ratio = side.get_pos().x/-top.get_pos().y
 
-const ENERGY_CONSUMPTION = 10
-const MIN_ENERGY = ENERGY_CONSUMPTION
-const MAX_ENERGY = 100
+var draw_queue = []
+
+export var ENERGY_CONSUMPTION = 10
+export var MAX_ENERGY = 100
+var MIN_ENERGY = ENERGY_CONSUMPTION
 
 var available_energy
 
@@ -14,11 +20,11 @@ signal on_hotspot_exit(hotspot)
 func _ready():
 	randomize()
 	available_energy = randi()%MAX_ENERGY+MIN_ENERGY
-
+	
 func _on_hotspot_body_enter( body ):
+	print('kakakaka')
 	if (body.get_name() == "player"):
 		emit_signal("on_hotspot_enter", self)
-
 
 func _on_hotspot_body_exit( body ):
 	if (body.get_name() == "player"):
@@ -38,4 +44,13 @@ func consume_energy(energy):
 
 func signal_strength(otherTransform):
 	# Deberia devolver un % (si los centros coinciden 100%)
-	return 100.0
+	var screen_distance = otherTransform.get_global_pos()-get_pos()
+	var distance = Vector2().distance_to(Vector2( screen_distance.x , screen_distance.y * ratio))
+	draw_queue.push_front(Rect2(Vector2(),Vector2( screen_distance.x , screen_distance.y * ratio)))
+	update()
+	return (distance/side.get_pos().x) * 100.0
+	
+func _draw():
+	for rect in draw_queue:
+		draw_line(rect.pos,rect.pos+rect.size,Color(0,255,0))
+	draw_queue = []
